ef336f13f3f2ba46e14548d88ce07e71af87ddd0
Henry <hgriffy@student.cscc.edu>
2026-06-28 21:43:48 -0400

    bug fixes -> working (maybe)

--- dice.py
+++ dice.py
@@ -4,13 +4,16 @@ class Dice():
     """simple class to represent a die of varying sides"""
     def __init__(self) -> None:
         self.sides: int = 6
-    
+
+    def set_sides(self,sides):
+        self.sides = sides
+
     def roll_die(self):
         return random.randint(1,self.sides)
 
 def main():
     """function to control die rolling"""
-    die: Dice = Dice(6)
-    print (roll_die())
+    die: Dice = Dice()
+    print (die.roll_die())
 
 main()
